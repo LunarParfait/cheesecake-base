67a7ff46b79fb831b81c503b4559062a8bbd0d2f
emily <snri.atomoxetine@proton.me>
2025-04-06 21:30:18 -0300

    added .keep files and routing DSL

--- app/controllers/lib.rs
+++ app/controllers/lib.rs
@@ -1,12 +1,12 @@
 use axum::Router;
+use axum::routing::*;
 use config::app::app_state::AppState;
 use config::create_routes;
 
 pub mod index;
-use index::IndexController;
 
 pub fn router() -> Router<AppState> {
     create_routes! {
-        IndexController
+        get "/" => index::render,
     }
 }
